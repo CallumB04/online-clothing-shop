197c566e0999c871fe5806aa343046584221ad5a
Cal <callumburgoyne04@gmail.com>
2025-09-15 17:00:42 +0100

    feat: start server on program startup

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/callumb04/online-clothing-shop/backend/internal/api"
+)
 
 func main() {
+	// Create new multiplexer and register handlers to routes.
+	mux := http.NewServeMux()
+	api.RegisterHandlers(mux)
+
+	// Start server on port 8080.
 	fmt.Println("Server is online!")
+	http.ListenAndServe(":8080", mux)
 }
