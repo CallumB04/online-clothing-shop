02be2db5c27500581d9f511a51f7ce599bcb84f7
Cal <callumburgoyne04@gmail.com>
2025-09-15 02:39:23 +0100

    feat: update item data structure

--- backend/internal/models/item.go
+++ backend/internal/models/item.go
@@ -1,10 +1,17 @@
 package models
 
 type Item struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-	Stock       int     `json:"stock"`
-	ImageURL    string  `json:"imageURL"`
+	ID         int             `json:"id"`
+	Name       string          `json:"name"`
+	Price      float64         `json:"price"`
+	Variations []ItemVariation `json:"variations"`
+}
+
+type ItemVariation struct {
+	ID           int      `json:"id"`
+	Name         string   `json:"name"`
+	PreviewColor string   `json:"previewColor"`
+	Stock        int      `json:"stock"`
+	ImageURL     string   `json:"imageURL"`
+	Sizes        []string `json:"sizes"` // ["XS", "S", "M", "L", "XL", "2XL"]
 }
