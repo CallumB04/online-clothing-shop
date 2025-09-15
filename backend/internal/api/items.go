cbbed1f82f40c63784fe5290b1261a4fd61000fe
Cal <callumburgoyne04@gmail.com>
2025-09-15 17:17:41 +0100

    feat: create handler for items endpoint

---
+++ backend/internal/api/items.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/callumb04/online-clothing-shop/backend/internal/data"
+	"github.com/callumb04/online-clothing-shop/backend/internal/util"
+)
+
+func handlerGetItems() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		items, err := data.LoadItems()
+
+		if err != nil {
+			util.ErrorResponse(w, http.StatusInternalServerError, "failed to loads items")
+		}
+
+		util.JSONResponse(w, http.StatusOK, items)
+	}
+}
