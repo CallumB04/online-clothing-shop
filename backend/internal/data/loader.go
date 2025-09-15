0e4613de3f3534b87208b1ae40f2d2d26b9922c6
Cal <callumburgoyne04@gmail.com>
2025-09-15 16:42:16 +0100

    feat: build items data loader

---
+++ backend/internal/data/loader.go
@@ -0,0 +1,28 @@
+package data
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/callumb04/online-clothing-shop/backend/internal/models"
+)
+
+const itemsFilePath string = "data/items.json"
+
+// Read items.json and return a slice of Item objects.
+func LoadItems() ([]models.Item, error) {
+	// Read data from file.
+	data, err := os.ReadFile(itemsFilePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Create slice for items and parse raw json data.
+	var items []models.Item
+	if err := json.Unmarshal(data, &items); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
