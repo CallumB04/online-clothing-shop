83c6a1c8fa6023aded02ab3a301b716baa25eb46
Cal <callumburgoyne04@gmail.com>
2025-09-27 04:00:05 +0100

    feat: add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,16 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { fetchItems, type Item } from "./api";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import ShopPage from "./pages/shop/ShopPage";
 
 function App() {
-    const [items, setItems] = useState<Item[]>([]);
-
-    // Fetch items from API
-    useEffect(() => {
-        const fetchData = async (): Promise<void> => {
-            let data = await fetchItems();
-            setItems(data);
-        };
-
-        fetchData();
-    }, []);
-
     return (
-        <main>
-            <h1 className="text-blue-500">Hello, world</h1>
-            <div className="flex gap-4">
-                {items.map((item) => (
-                    <div key={item.id}>
-                        <h2>{item.name}</h2>
-                        <p>{item.priceGBP}</p>
-                    </div>
-                ))}
-            </div>
-        </main>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/shop" element={<ShopPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
