df12c05092b1277e030c136d95c43f753a24b178
Cal <callumburgoyne04@gmail.com>
2025-09-27 04:17:48 +0100

    feat: add button component

---
+++ frontend/src/components/button/Button.tsx
@@ -0,0 +1,25 @@
+interface ButtonProps {
+    children: React.ReactNode;
+    onClick?: () => void;
+    disabled?: boolean;
+    className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    children,
+    onClick,
+    disabled,
+    className,
+}) => {
+    return (
+        <button
+            className={`${!disabled && "cursor-pointer"}` + " " + className}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
