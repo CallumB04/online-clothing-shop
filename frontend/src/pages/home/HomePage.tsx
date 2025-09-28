656df17521dde0619c9d9c3d71477b51a8e59fbe
Cal <callumburgoyne04@gmail.com>
2025-09-28 03:25:34 +0100

    chore: add buttons for testing

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+import UIButton from "../../components/button/UIButton";
+import PrimaryButton from "../../components/button/PrimaryButton";
+
 const HomePage = () => {
     return (
         <main>
             <h1 className="text-red-500">Home Page</h1>
+            <Link to="/shop">
+                <PrimaryButton>Go to Shop</PrimaryButton>
+            </Link>
+            <UIButton onClick={() => alert("Hello")}>Buy Now</UIButton>
         </main>
     );
 };
