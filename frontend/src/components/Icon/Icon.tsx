e2afd43dbef0aa6ddb71b7d432d346c6e2a4936c
Cal <callumburgoyne04@gmail.com>
2025-09-28 19:28:14 +0100

    feat: add icon component

---
+++ frontend/src/components/Icon/Icon.tsx
@@ -0,0 +1,17 @@
+interface IconProps {
+    icon: string;
+    onClick?: () => void;
+}
+
+const Icon: React.FC<IconProps> = ({ icon, onClick }) => {
+    return (
+        <i
+            className={`material-symbols-outlined ${onClick && "cursor-pointer"}`}
+            onClick={onClick}
+        >
+            {icon}
+        </i>
+    );
+};
+
+export default Icon;
