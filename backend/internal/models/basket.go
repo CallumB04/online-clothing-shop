aef2a0b2bcb367f3eb6f5b04ee81d0dda55c7f92
Cal <callumburgoyne04@gmail.com>
2025-09-28 19:39:33 +0100

    feat: update basket type

--- backend/internal/models/basket.go
+++ backend/internal/models/basket.go
@@ -1,13 +1,10 @@
 package models
 
-type Basket struct {
-	Items    []BasketItem `json:"items"`
-	Discount float64      `json:"discount"` // 0.1 -> 10%
-}
+type Basket = []BasketItem
 
 type BasketItem struct {
-	ItemID      int    `json:"itemID"`
-	VariationID int    `json:"variationID"`
+	ItemID      string `json:"itemID"`
+	VariationID string `json:"variationID"`
 	Size        string `json:"size"`
 	Quantity    int    `json:"quantity"`
 }
