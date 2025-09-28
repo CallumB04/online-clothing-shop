5da554bcdbc4bcfa6819404e94e50176697b1afa
Cal <callumburgoyne04@gmail.com>
2025-09-28 03:42:57 +0100

    feat: add navbar to router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import Navbar from "./layout/Navbar";
 
 function App() {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/shop" element={<ShopPage />} />
