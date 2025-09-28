5e4d8eeeb60b8fbd2199ecab5324ae7f833e2935
CallumB04 <callumburgoyne04@gmail.com>
2025-09-28 04:49:54 +0100

    feat: create navbar item component

---
+++ frontend/src/layout/navbar/NavbarItem.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+interface NavbarItemProps {
+    label: string;
+    toLocation: string;
+}
+
+const NavbarItem: React.FC<NavbarItemProps> = ({ label, toLocation }) => {
+    return (
+        <Link to={toLocation}>
+            <p className="text-charcoal font-bold">{label}</p>
+        </Link>
+    );
+};
+
+export default NavbarItem;
