e3397ac4d615d8defd6c73eae5f400235d760e9e
CallumB04 <callumburgoyne04@gmail.com>
2025-09-28 04:50:21 +0100

    feat: add initial items to navbar

--- frontend/src/layout/navbar/Navbar.tsx
+++ frontend/src/layout/navbar/Navbar.tsx
@@ -1,6 +1,15 @@
+import NavbarItem from "./NavbarItem";
+import NavbarLogo from "./NavbarLogo";
+
 const Navbar = () => {
     return (
-        <nav className="fixed h-navbar-height bg-background w-screen top-0 left-0"></nav>
+        <nav className="fixed h-navbar-height bg-background w-screen top-0 left-0 flex items-center px-16 gap-12 border-b-1 border-b-[#eaeaea]">
+            <NavbarLogo />
+            <NavbarItem label="Men's" toLocation="/shop" />
+            <NavbarItem label="Women's" toLocation="/shop" />
+            <NavbarItem label="New Arrivals" toLocation="/shop" />
+            <NavbarItem label="Offers" toLocation="/shop" />
+        </nav>
     );
 };
 
