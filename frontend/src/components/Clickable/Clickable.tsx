0332a4252d20e59dbce54eb274e2a67152b967b2
Cal <callumburgoyne04@gmail.com>
2025-09-29 11:56:54 +0100

    feat: add global component for clickables

---
+++ frontend/src/components/Clickable/Clickable.tsx
@@ -0,0 +1,22 @@
+interface ClickableProps {
+    children: React.ReactNode;
+    onClick: () => void;
+    className?: string;
+}
+
+const Clickable: React.FC<ClickableProps> = ({
+    children,
+    onClick,
+    className,
+}) => {
+    return (
+        <span
+            className={`inline-flex items-center gap-1 p-1 ${className} clickable-hovered cursor-pointer`}
+            onClick={onClick}
+        >
+            {children}
+        </span>
+    );
+};
+
+export default Clickable;
