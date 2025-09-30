71cb8de44f7dddd146ed301e709e4d108ad23e6d
Cal <callumburgoyne04@gmail.com>
2025-10-01 00:55:01 +0100

    feat: add empty sidebar component

---
+++ frontend/src/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return (
+        <aside className="w-sidebar-width bg-background fixed top-0 left-0 z-10 hidden h-screen border-r-1 border-r-[#eaeaea] lg:flex">
+            This is a sidebar
+        </aside>
+    );
+};
+
+export default Sidebar;
