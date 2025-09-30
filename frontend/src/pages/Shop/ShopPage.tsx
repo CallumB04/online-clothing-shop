81ed85819828a0987d67d18ec55c9bb973e576c4
Cal <callumburgoyne04@gmail.com>
2025-10-01 00:55:13 +0100

    feat: add sidebar to shop page

--- frontend/src/pages/Shop/ShopPage.tsx
+++ frontend/src/pages/Shop/ShopPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchItems, type Item } from "../../api";
+import Sidebar from "../../layout/Sidebar/Sidebar";
 
 const ShopPage = () => {
     const [items, setItems] = useState<Item[]>([]);
@@ -15,17 +16,20 @@ const ShopPage = () => {
     }, []);
 
     return (
-        <main>
-            <h1 className="text-blue-500">Shop Page</h1>
-            <div className="flex gap-4">
-                {items.map((item) => (
-                    <div key={item.id}>
-                        <h2>{item.name}</h2>
-                        <p>{item.priceGBP}</p>
-                    </div>
-                ))}
-            </div>
-        </main>
+        <>
+            <Sidebar />
+            <main className="lg:ml-sidebar-width">
+                <h1 className="text-blue-500">Shop Page</h1>
+                <div className="flex gap-4">
+                    {items.map((item) => (
+                        <div key={item.id}>
+                            <h2>{item.name}</h2>
+                            <p>{item.priceGBP}</p>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </>
     );
 };
 
