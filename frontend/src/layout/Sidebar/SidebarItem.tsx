98d473d7a6a8ab71d03127fce1b134859f81bd54
Cal <callumburgoyne04@gmail.com>
2025-10-01 21:34:50 +0100

    feat: add sidebar item component

---
+++ frontend/src/layout/Sidebar/SidebarItem.tsx
@@ -0,0 +1,27 @@
+import { Link, useLocation } from "react-router-dom";
+import Icon from "../../components/Icon/Icon";
+
+interface SidebarItemProps {
+    label: string;
+    category: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({ label, category }) => {
+    const location = useLocation();
+
+    return (
+        <Link
+            to={`/shop?category=${category}`}
+            className={`flex w-full items-center justify-between rounded-md px-3 py-2 transition-shadow duration-150 hover:shadow-sm ${location.search.includes(category) && "bg-[#f3f3f3] shadow-xs"}`}
+        >
+            <p
+                className={`font-primary text-charcoal ${location.search.includes(category) && "font-semibold"}`}
+            >
+                {label}
+            </p>
+            <Icon icon="chevron_right" />
+        </Link>
+    );
+};
+
+export default SidebarItem;
