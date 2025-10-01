3f4d0b5ef7899bf71c45b8ef26ff60d51d151de5
Cal <callumburgoyne04@gmail.com>
2025-10-01 22:15:25 +0100

    feat: add breadcrumbs and update items in sidebar

--- frontend/src/layout/Sidebar/Sidebar.tsx
+++ frontend/src/layout/Sidebar/Sidebar.tsx
@@ -1,13 +1,29 @@
+import SidebarBreadcrumbs from "./SidebarBreadcrumbs";
 import SidebarItem from "./SidebarItem";
 
-const Sidebar = () => {
+interface SidebarProps {
+    gender: string | undefined;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ gender }) => {
     return (
         <aside className="w-sidebar-width bg-background pt-navbar-height fixed top-0 left-0 z-10 hidden h-screen border-r-1 border-r-[#eaeaea] lg:flex">
-            <div className="flex w-full flex-col gap-2 p-4">
-                <SidebarItem label="New" category="new" />
-                <SidebarItem label="Tops" category="tops" />
-                <SidebarItem label="Bottoms" category="bottoms" />
-                <SidebarItem label="Hoodies" category="hoodies" />
+            <div className="flex w-full flex-col gap-6 p-4">
+                <SidebarBreadcrumbs gender={gender} />
+                <div className="flex w-full flex-col gap-2">
+                    <SidebarItem label="New" category="new" gender={gender} />
+                    <SidebarItem label="Tops" category="tops" gender={gender} />
+                    <SidebarItem
+                        label="Bottoms"
+                        category="bottoms"
+                        gender={gender}
+                    />
+                    <SidebarItem
+                        label="Hoodies"
+                        category="hoodies"
+                        gender={gender}
+                    />
+                </div>
             </div>
         </aside>
     );
