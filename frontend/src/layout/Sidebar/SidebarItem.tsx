892494d314ab5e29de47666e04401c2b820666c2
Cal <callumburgoyne04@gmail.com>
2025-10-01 22:15:09 +0100

    feat: pass gender and category into redirect

--- frontend/src/layout/Sidebar/SidebarItem.tsx
+++ frontend/src/layout/Sidebar/SidebarItem.tsx
@@ -4,14 +4,23 @@ import Icon from "../../components/Icon/Icon";
 interface SidebarItemProps {
     label: string;
     category: string;
+    gender: string | undefined;
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ label, category }) => {
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    label,
+    category,
+    gender,
+}) => {
     const location = useLocation();
 
     return (
         <Link
-            to={`/shop?category=${category}`}
+            to={
+                gender
+                    ? `/shop/${gender}?category=${category}`
+                    : `/shop?category=${category}`
+            }
             className={`flex w-full items-center justify-between rounded-md px-3 py-2 transition-shadow duration-150 hover:shadow-sm ${location.search.includes(category) && "bg-[#f3f3f3] shadow-xs"}`}
         >
             <p
