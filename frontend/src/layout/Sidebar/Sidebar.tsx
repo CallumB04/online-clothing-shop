d575e797e7b8633d474474b53e9109eaafe725ca
Cal <callumburgoyne04@gmail.com>
2025-10-01 21:35:01 +0100

    feat: add test sidebar items

--- frontend/src/layout/Sidebar/Sidebar.tsx
+++ frontend/src/layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,14 @@
+import SidebarItem from "./SidebarItem";
+
 const Sidebar = () => {
     return (
-        <aside className="w-sidebar-width bg-background fixed top-0 left-0 z-10 hidden h-screen border-r-1 border-r-[#eaeaea] lg:flex">
-            This is a sidebar
+        <aside className="w-sidebar-width bg-background pt-navbar-height fixed top-0 left-0 z-10 hidden h-screen border-r-1 border-r-[#eaeaea] lg:flex">
+            <div className="flex w-full flex-col gap-2 p-4">
+                <SidebarItem label="New" category="new" />
+                <SidebarItem label="Tops" category="tops" />
+                <SidebarItem label="Bottoms" category="bottoms" />
+                <SidebarItem label="Hoodies" category="hoodies" />
+            </div>
         </aside>
     );
 };
