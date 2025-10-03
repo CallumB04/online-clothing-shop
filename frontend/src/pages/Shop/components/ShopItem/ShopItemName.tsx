4146577fea3b117c207e178c8b0d09377745a29e
Cal <callumburgoyne04@gmail.com>
2025-10-03 13:28:40 +0100

    feat: update font sizing

--- frontend/src/pages/Shop/components/ShopItem/ShopItemName.tsx
+++ frontend/src/pages/Shop/components/ShopItem/ShopItemName.tsx
@@ -3,7 +3,11 @@ interface ShopItemNameProps {
 }
 
 const ShopItemName: React.FC<ShopItemNameProps> = ({ name }) => {
-    return <p className="font-primary text-charcoal text-sm">{name}</p>;
+    return (
+        <p className="font-primary text-charcoal text-xs sm:text-sm lg:text-xs xl:text-sm">
+            {name}
+        </p>
+    );
 };
 
 export default ShopItemName;
--- frontend/src/pages/Shop/components/ShopItem/ShopItemPrice.tsx
+++ frontend/src/pages/Shop/components/ShopItem/ShopItemPrice.tsx
@@ -4,7 +4,7 @@ interface ShopItemPriceProps {
 
 const ShopItemPrice: React.FC<ShopItemPriceProps> = ({ price }) => {
     return (
-        <p className="font-primary text-charcoal text-sm font-semibold">
+        <p className="font-primary text-charcoal text-xs font-semibold sm:text-sm lg:text-xs xl:text-sm">
             £{price}
         </p>
     );
