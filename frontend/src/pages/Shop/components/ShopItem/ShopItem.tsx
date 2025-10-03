3ded17b550cbc16a7fe95bb674ec58280b2087ab
Cal <callumburgoyne04@gmail.com>
2025-10-03 21:45:45 +0100

    feat: update padding on shop item

--- frontend/src/pages/Shop/components/ShopItem/ShopItem.tsx
+++ frontend/src/pages/Shop/components/ShopItem/ShopItem.tsx
@@ -14,7 +14,7 @@ const ShopItem: React.FC<ShopItemProps> = ({ item }) => {
             className="group flex cursor-pointer flex-col gap-2 rounded-md transition-all duration-200 hover:scale-105 hover:shadow-lg"
         >
             <ShopItemImage imageURL={item.variations[0].imageURL} />
-            <div className="flex flex-col gap-1 p-4 pt-0">
+            <div className="flex flex-col gap-1 px-3 py-4 pt-0">
                 <ShopItemName name={item.name} />
                 <ShopItemPrice price={item.priceGBP} />
             </div>
