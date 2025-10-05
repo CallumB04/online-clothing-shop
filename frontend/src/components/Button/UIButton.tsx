6626a0af0531e957bf0a63ce8833992432b36b1c
Cal <callumburgoyne04@gmail.com>
2025-10-05 14:54:56 +0100

    feat: increase padding on ui button

--- frontend/src/components/Button/UIButton.tsx
+++ frontend/src/components/Button/UIButton.tsx
@@ -19,7 +19,7 @@ const UIButton: React.FC<UIButtonProps> = ({
         <Button
             onClick={onClick}
             disabled={disabled}
-            className={`border-charcoal font-primary gap-2 rounded-sm border-[1px] px-2 py-1 font-light ${fullWidth ? "w-full" : "w-max"} ${className}`}
+            className={`border-charcoal font-primary gap-2 rounded-sm border-[1px] px-2 py-1.5 font-light ${fullWidth ? "w-full" : "w-max"} ${className}`}
         >
             {children}
         </Button>
