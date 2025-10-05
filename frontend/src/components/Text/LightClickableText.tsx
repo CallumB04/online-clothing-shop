7f6e8de8086e0c25dbd34adc11af367adb6b3743
Cal <callumburgoyne04@gmail.com>
2025-10-05 03:29:50 +0100

    feat: add component for light clickable text

---
+++ frontend/src/components/Text/LightClickableText.tsx
@@ -0,0 +1,20 @@
+interface LightClickableTextProps {
+    text: string;
+    onClick?: () => void;
+}
+
+const LightClickableText: React.FC<LightClickableTextProps> = ({
+    text,
+    onClick,
+}) => {
+    return (
+        <p
+            className="text-light-text font-primary border-b-light-text hover:text-charcoal hover:border-b-charcoal border-b-1 text-sm leading-4 transition-colors duration-200"
+            onClick={onClick}
+        >
+            {text}
+        </p>
+    );
+};
+
+export default LightClickableText;
