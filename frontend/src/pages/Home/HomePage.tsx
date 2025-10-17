d0a78a77935dbde11b01a741c985bde90fccb975
Cal <callumburgoyne04@gmail.com>
2025-10-17 01:44:15 +0100

    feat: add sidebar component to home page

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -2,19 +2,23 @@ import { Link } from "react-router-dom";
 import UIButton from "@/components/Button/UIButton";
 import PrimaryButton from "@/components/Button/PrimaryButton";
 import Clickable from "@/components/Clickable/Clickable";
+import HomeSidebarMobile from "@/layout/HomeSidebarMobile/HomeSidebarMobile";
 
 const HomePage = () => {
     return (
-        <main className="flex flex-col gap-4 px-2 py-4 sm:px-4 sm:py-6">
-            <h1 className="text-red-500">Home Page</h1>
-            <Link to="/shop">
-                <PrimaryButton>Go to Shop</PrimaryButton>
-            </Link>
-            <UIButton onClick={() => alert("Hello")}>Buy Now</UIButton>
-            <Clickable onClick={() => alert("Click")}>
-                This is a clickable
-            </Clickable>
-        </main>
+        <>
+            <HomeSidebarMobile />
+            <main className="flex flex-col gap-4 px-2 py-4 sm:px-4 sm:py-6">
+                <h1 className="text-red-500">Home Page</h1>
+                <Link to="/shop">
+                    <PrimaryButton>Go to Shop</PrimaryButton>
+                </Link>
+                <UIButton onClick={() => alert("Hello")}>Buy Now</UIButton>
+                <Clickable onClick={() => alert("Click")}>
+                    This is a clickable
+                </Clickable>
+            </main>
+        </>
     );
 };
 
