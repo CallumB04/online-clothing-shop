cd620282a2416a8a43827c373a83e1dd535988b5
Cal <callumburgoyne04@gmail.com>
2025-10-17 01:43:31 +0100

    feat: add props for if sidebar visible of different devices

--- frontend/src/layout/Sidebar/Sidebar.tsx
+++ frontend/src/layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,18 @@
 interface SidebarProps {
     children: React.ReactNode;
+    visibleMobile?: boolean;
+    visibleDesktop?: boolean;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+const Sidebar: React.FC<SidebarProps> = ({
+    children,
+    visibleMobile,
+    visibleDesktop,
+}) => {
     return (
-        <aside className="w-sidebar-width bg-background pt-navbar-height fixed top-0 left-0 z-10 hidden h-screen border-r-1 border-r-[#eaeaea] lg:flex">
+        <aside
+            className={`w-sidebar-width bg-background pt-navbar-height fixed top-0 left-0 z-10 ${visibleMobile ? "flex" : "hidden"} h-screen border-r-1 border-r-[#eaeaea] ${visibleDesktop ? "lg:flex" : "lg:hidden"}`}
+        >
             <div className="flex w-full flex-col gap-6 p-4">{children}</div>
         </aside>
     );
