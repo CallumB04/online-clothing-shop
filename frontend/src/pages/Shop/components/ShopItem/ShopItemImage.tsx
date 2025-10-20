74da1d9a4b4d199fd27ca6391283813a52c73622
Cal <callumburgoyne04@gmail.com>
2025-10-20 05:51:26 +0100

    feat: add preview button to shop item image

--- frontend/src/pages/Shop/components/ShopItem/ShopItemImage.tsx
+++ frontend/src/pages/Shop/components/ShopItem/ShopItemImage.tsx
@@ -1,3 +1,4 @@
+import Icon from "@/components/Icon/Icon";
 import { Link } from "react-router-dom";
 
 interface ShopItemImageProps {
@@ -13,12 +14,20 @@ const ShopItemImage: React.FC<ShopItemImageProps> = ({
 }) => {
     return (
         <Link
+            className="relative"
             to={`/item/${itemID}${variationID !== "0" ? "?variation=" + variationID : ""}`}
         >
             <img
                 src={imageURL}
                 className="aspect-[3/4] w-full cursor-pointer rounded-md group-hover:rounded-b-none"
             />
+            {/* Preview button */}
+            <div className="bg-background/75 hover:bg-background absolute bottom-6 left-1/2 hidden w-11/12 -translate-x-1/2 items-center justify-center gap-1 rounded-full py-2 shadow transition-colors duration-300 group-hover:flex">
+                <p className="font-primary text-charcoal text-sm font-semibold">
+                    Preview
+                </p>
+                <Icon icon="open_in_full" className="scale-75" />
+            </div>
         </Link>
     );
 };
