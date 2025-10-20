bc956a4dae39fa657f9f4622c7358f360785f7a6
Cal <callumburgoyne04@gmail.com>
2025-10-20 06:08:44 +0100

    feat: allow popup to be closed by clicking background

--- frontend/src/components/Popup/Popup.tsx
+++ frontend/src/components/Popup/Popup.tsx
@@ -1,11 +1,18 @@
 interface PopupProps {
     children: React.ReactNode;
+    closePopup: () => void;
 }
 
-const Popup: React.FC<PopupProps> = ({ children }) => {
+const Popup: React.FC<PopupProps> = ({ children, closePopup }) => {
     return (
-        <dialog className="fixed top-0 left-0 z-99 flex h-screen w-screen items-center justify-center bg-[#00000044] px-4">
-            <div className="w-max rounded-lg bg-white p-4 shadow">
+        <dialog
+            className="fixed top-0 left-0 z-99 flex h-screen w-screen items-center justify-center bg-[#00000044] px-4"
+            onMouseDown={closePopup}
+        >
+            <div
+                className="w-max rounded-lg bg-white p-4 shadow"
+                onMouseDown={(e) => e.stopPropagation()}
+            >
                 {children}
             </div>
         </dialog>
