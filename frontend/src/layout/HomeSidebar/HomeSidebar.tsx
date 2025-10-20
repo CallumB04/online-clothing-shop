00b793e95b1b1bffe296f420891e2ca36422e2cc
Cal <callumburgoyne04@gmail.com>
2025-10-20 05:12:08 +0100

    feat: create basic home sidebar

--- frontend/src/layout/HomeSidebar/HomeSidebar.tsx
+++ frontend/src/layout/HomeSidebar/HomeSidebar.tsx
@@ -1,11 +1,34 @@
 import Sidebar from "../Sidebar/Sidebar";
+import SidebarItems from "../Sidebar/SidebarItems";
+import SidebarItem from "./SidebarItem";
 
 interface HomeSidebarProps {
     open?: boolean;
 }
 
 const HomeSidebar: React.FC<HomeSidebarProps> = ({ open }) => {
-    return <Sidebar visibleMobile={open}>This is home sidebar</Sidebar>;
+    return (
+        <Sidebar visibleMobile={open}>
+            <SidebarItems>
+                <SidebarItem label="Home" to="/" icon="home" />
+                <SidebarItem label="Browse" to="/shop" icon="apparel" />
+                <SidebarItem
+                    label="Offers"
+                    to="/shop?category=offers"
+                    icon="shoppingmode"
+                />
+                <SidebarItem
+                    label="New Arrivals"
+                    to="/shop?category=new"
+                    icon="acute"
+                />
+            </SidebarItems>
+            <SidebarItems>
+                <SidebarItem label="Men's" to="/shop/mens" icon="male" />
+                <SidebarItem label="Women's" to="/shop/womens" icon="female" />
+            </SidebarItems>
+        </Sidebar>
+    );
 };
 
 export default HomeSidebar;
