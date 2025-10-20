02aa4da7b089a3fc1409736e91ab9629d1c469aa
Cal <callumburgoyne04@gmail.com>
2025-10-20 06:30:18 +0100

    feat: add minimum gap between popup title and close icon

--- frontend/src/components/Popup/PopupHeader.tsx
+++ frontend/src/components/Popup/PopupHeader.tsx
@@ -14,7 +14,7 @@ const PopupHeader: React.FC<PopupHeaderProps> = ({
     closePopup,
 }) => {
     return (
-        <span className="flex w-full justify-between">
+        <span className="flex w-full justify-between gap-12">
             <div className="flex flex-col gap-0.5">
                 {title && <PopupTitle title={title} />}
                 {description && <PopupDescription description={description} />}
