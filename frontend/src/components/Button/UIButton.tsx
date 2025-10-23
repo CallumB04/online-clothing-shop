cf51b9f02cc20d7694de9ee7e5d5084fe7a03576
Cal <callumburgoyne04@gmail.com>
2025-10-23 23:21:55 +0100

    feat: add danger option for ui buttons

--- frontend/src/components/Button/UIButton.tsx
+++ frontend/src/components/Button/UIButton.tsx
@@ -6,6 +6,7 @@ interface UIButtonProps {
     disabled?: boolean;
     className?: string;
     fullWidth?: boolean;
+    danger?: boolean; // is button a cancel/danger button, red hover styles
 }
 
 const UIButton: React.FC<UIButtonProps> = ({
@@ -14,12 +15,13 @@ const UIButton: React.FC<UIButtonProps> = ({
     disabled,
     className,
     fullWidth,
+    danger,
 }) => {
     return (
         <Button
             onClick={onClick}
             disabled={disabled}
-            className={`${disabled ? "border-ui-button-disabled text-ui-button-disabled" : "border-charcoal text-charcoal"} ${!disabled && "hover:border-ui-button-hover hover:text-ui-button-hover"} font-primary gap-2 rounded-md border-[1px] px-2 py-1.5 transition-colors duration-150 ${fullWidth ? "w-full" : "w-max"} ${className}`}
+            className={`${disabled ? "border-ui-button-disabled text-ui-button-disabled" : "border-charcoal text-charcoal"} ${!disabled && (danger ? "hover:border-danger hover:text-danger" : "hover:border-ui-button-hover hover:text-ui-button-hover")} font-primary gap-2 rounded-md border-[1px] px-2 py-1.5 transition-colors duration-150 ${fullWidth ? "w-full" : "w-max"} ${className}`}
         >
             {children}
         </Button>
