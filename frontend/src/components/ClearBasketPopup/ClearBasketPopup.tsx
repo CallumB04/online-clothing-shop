698783abc570e64d692b7905ced6775cf83d7292
Cal <callumburgoyne04@gmail.com>
2025-10-26 00:14:46 +0100

    feat: add functionality to popup buttons

--- frontend/src/components/ClearBasketPopup/ClearBasketPopup.tsx
+++ frontend/src/components/ClearBasketPopup/ClearBasketPopup.tsx
@@ -2,12 +2,20 @@ import Popup from "@/components/Popup/Popup";
 import PopupHeader from "../Popup/PopupHeader";
 import SecondaryButton from "../Button/SecondaryButton";
 import PrimaryButton from "../Button/PrimaryButton";
+import { useBasket } from "@/context/BasketContext";
 
 interface ClearBasketPopupProps {
     closePopup: () => void;
 }
 
 const ClearBasketPopup: React.FC<ClearBasketPopupProps> = ({ closePopup }) => {
+    const { clearBasket } = useBasket();
+
+    const handleClear = () => {
+        clearBasket();
+        closePopup();
+    };
+
     return (
         <Popup closePopup={closePopup}>
             <PopupHeader
@@ -16,10 +24,15 @@ const ClearBasketPopup: React.FC<ClearBasketPopupProps> = ({ closePopup }) => {
                 closePopup={closePopup}
             />
             <span className="flex w-full flex-1 gap-2">
-                <SecondaryButton className="w-full text-sm">
+                <SecondaryButton
+                    className="w-full text-sm"
+                    onClick={closePopup}
+                >
                     Cancel
                 </SecondaryButton>
-                <PrimaryButton className="w-full text-sm">Clear</PrimaryButton>
+                <PrimaryButton className="w-full text-sm" onClick={handleClear}>
+                    Clear
+                </PrimaryButton>
             </span>
         </Popup>
     );
