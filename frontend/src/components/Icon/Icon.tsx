dfaede7c9300b4b2d094e6aaeaa25ae74b9628f4
Cal <callumburgoyne04@gmail.com>
2025-10-25 14:33:56 +0100

    feat: add title property to icon component

--- frontend/src/components/Icon/Icon.tsx
+++ frontend/src/components/Icon/Icon.tsx
@@ -1,15 +1,23 @@
 interface IconProps {
     icon: string;
     color?: string;
+    title?: string;
     className?: string;
     onClick?: () => void;
 }
 
-const Icon: React.FC<IconProps> = ({ icon, color, className, onClick }) => {
+const Icon: React.FC<IconProps> = ({
+    icon,
+    color,
+    title,
+    className,
+    onClick,
+}) => {
     return (
         <i
             className={`material-symbols-outlined ${onClick && "cursor-pointer"} ${color ? color : "text-charcoal"} ${className}`}
             onClick={onClick}
+            title={title}
         >
             {icon}
         </i>
