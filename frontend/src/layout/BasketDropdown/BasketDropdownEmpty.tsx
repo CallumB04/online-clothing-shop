d2750e732ad7ca1ccf87cdd7e34cbc0f80f95d6b
Cal <callumburgoyne04@gmail.com>
2025-10-25 19:18:16 +0100

    feat: add component for empty basket text

---
+++ frontend/src/layout/BasketDropdown/BasketDropdownEmpty.tsx
@@ -0,0 +1,9 @@
+const BasketDropdownEmpty = () => {
+    return (
+        <p className="font-primary border-b-layout-border text-light-text mt-4 border-b-1 pb-4 text-center text-sm">
+            There are no items in your basket.
+        </p>
+    );
+};
+
+export default BasketDropdownEmpty;
