ae602751042d57d8c3dab4a5a92f100817ab86ac
Cal <callumburgoyne04@gmail.com>
2025-10-27 16:50:56 +0000

    chore: update gender from F to W for Womens

--- backend/internal/models/item.go
+++ backend/internal/models/item.go
@@ -3,7 +3,7 @@ package models
 type Item struct {
 	ID         string          `json:"id"`
 	Name       string          `json:"name"`
-	Gender     string          `json:"gender"` // "M" / "F"
+	Gender     string          `json:"gender"` // "M" / "W"
 	PriceGBP   float64         `json:"priceGBP"`
 	Variations []ItemVariation `json:"variations"`
 }
