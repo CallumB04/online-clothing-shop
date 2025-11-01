1252b13077ce56275dceafec212f5f5575b2cdd5
Cal <callumburgoyne04@gmail.com>
2025-11-01 16:49:12 +0000

    feat: add custom class prop

--- frontend/src/components/Popup/PopupButtons.tsx
+++ frontend/src/components/Popup/PopupButtons.tsx
@@ -1,9 +1,14 @@
 interface PopupButtonsProps {
     children: React.ReactNode;
+    className?: string;
 }
 
-const PopupButtons: React.FC<PopupButtonsProps> = ({ children }) => {
-    return <span className="flex w-full flex-1 gap-2">{children}</span>;
+const PopupButtons: React.FC<PopupButtonsProps> = ({ children, className }) => {
+    return (
+        <span className={`flex w-full flex-1 gap-2 ${className}`}>
+            {children}
+        </span>
+    );
 };
 
 export default PopupButtons;
