7a201aac8fb2d51b8dc3e322f755bb06f0f0fc65
Cal <callumburgoyne04@gmail.com>
2025-11-02 02:20:05 +0000

    feat: allow for onclick function

--- frontend/src/components/Text/DarkText.tsx
+++ frontend/src/components/Text/DarkText.tsx
@@ -1,9 +1,19 @@
 interface DarkTextProps {
     children: React.ReactNode;
     className?: string;
+    onClick?: () => void;
 }
-export const DarkText: React.FC<DarkTextProps> = ({ children, className }) => {
+export const DarkText: React.FC<DarkTextProps> = ({
+    children,
+    className,
+    onClick,
+}) => {
     return (
-        <p className={`font-primary text-charcoal ${className}`}>{children}</p>
+        <p
+            className={`font-primary text-charcoal ${className}`}
+            onClick={onClick}
+        >
+            {children}
+        </p>
     );
 };
--- frontend/src/components/Text/LightText.tsx
+++ frontend/src/components/Text/LightText.tsx
@@ -1,13 +1,18 @@
 interface LightTextProps {
     children: React.ReactNode;
     className?: string;
+    onClick?: () => void;
 }
 export const LightText: React.FC<LightTextProps> = ({
     children,
     className,
+    onClick,
 }) => {
     return (
-        <p className={`font-primary text-light-text ${className}`}>
+        <p
+            className={`font-primary text-light-text ${className}`}
+            onClick={onClick}
+        >
             {children}
         </p>
     );
