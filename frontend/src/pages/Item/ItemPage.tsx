9b550c42d87a84738a86935057cadee32346b562
Cal <callumburgoyne04@gmail.com>
2025-11-02 06:21:07 +0000

    feat: show not found if item doesnt exist

--- frontend/src/pages/Item/ItemPage.tsx
+++ frontend/src/pages/Item/ItemPage.tsx
@@ -2,6 +2,7 @@ import { fetchItemById, type Item } from "@/api";
 import DefaultSidebar from "@/layout/DefaultSidebar/DefaultSidebar";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import NotFoundPage from "../NotFound/NotFoundPage";
 
 interface ItemPageProps {
     isMobileSidebarOpen?: boolean;
@@ -9,22 +10,31 @@ interface ItemPageProps {
 
 const ItemPage: React.FC<ItemPageProps> = ({ isMobileSidebarOpen }) => {
     const [item, setItem] = useState<Item | undefined>(undefined);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const { id } = useParams(); // get item id from url
 
+    // fetching item using id in url
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true);
             if (id) {
                 const resp = await fetchItemById(id);
 
                 if (resp) {
                     setItem(resp);
                 }
+                setIsLoading(false);
             }
         };
 
         fetchData();
     }, [id]);
+
+    // if tried to fetch item and doesnt exist, show not found page
+    if (!isLoading && !item) {
+        return <NotFoundPage isMobileSidebarOpen={isMobileSidebarOpen} />;
+    }
 
     return (
         <>
