ab8a9a77e23867d10b570ab4b3c05b022e1dc469
Cal <callumburgoyne04@gmail.com>
2025-11-05 11:19:16 +0000

    feat: add component for dropdown

---
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,49 @@
+import { DarkText } from "../Text/DarkText";
+
+type DropdownEntry = {
+    value: string;
+    display: string;
+};
+
+interface DropdownProps {
+    label?: string;
+    options: DropdownEntry[];
+    defaultValue?: string;
+    className?: string;
+    fullWidth?: boolean;
+    mobileFullWidth?: boolean;
+    onChange?: () => void;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+    label,
+    options,
+    defaultValue,
+    className,
+    fullWidth,
+    mobileFullWidth,
+    onChange,
+}) => {
+    return (
+        <div
+            className={`flex flex-col gap-1 ${fullWidth && "w-full"} ${mobileFullWidth && "w-full sm:w-max"}`}
+        >
+            {label && (
+                <DarkText className="pl-1 text-xs font-semibold">
+                    {label}
+                </DarkText>
+            )}
+            <select
+                className={`border-input-border text-charcoal font-primary flex h-10 rounded-md border-1 pr-8 pl-4 text-sm outline-none ${className} ${fullWidth && "w-full"} ${mobileFullWidth && "w-full sm:w-max"}`}
+                onChange={onChange}
+                defaultValue={defaultValue}
+            >
+                {options.map((o) => (
+                    <option value={o.value}>{o.display}</option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default Dropdown;
