8f4479dca8f1eedd079c98d2ea4f7fc5c17fcffb
Cal <callumburgoyne04@gmail.com>
2025-11-05 00:24:16 +0000

    feat: add component for text input

---
+++ frontend/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,30 @@
+interface TextInputProps {
+    fullWidth?: boolean;
+    placeholder?: string;
+    className?: string;
+    onChange?: (input: string) => void;
+}
+
+const TextInput: React.FC<TextInputProps> = ({
+    fullWidth = false,
+    placeholder = "Type here...",
+    className,
+    onChange,
+}) => {
+    const handleChange = (newInput: string) => {
+        if (onChange) {
+            onChange(newInput); // call prop function to pass input value to parent
+        }
+    };
+
+    return (
+        <input
+            type="text"
+            placeholder={placeholder}
+            className={`border-input-border text-charcoal font-primary flex h-10 rounded-md border-1 px-4 text-sm outline-none ${fullWidth ? "w-full" : "w-56"} ${className}`}
+            onChange={(e) => handleChange(e.target.value)}
+        />
+    );
+};
+
+export default TextInput;
