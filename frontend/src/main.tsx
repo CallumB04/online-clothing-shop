6c51ffe9aabba4c95828ca95d4faf0ae9afe3da4
Cal <callumburgoyne04@gmail.com>
2025-11-06 23:55:39 +0000

    feat: add toaster provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BasketProvider } from "./context/BasketContext.tsx";
+import { ToasterProvider } from "./context/ToasterContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BasketProvider>
-            <App />
-        </BasketProvider>
+        <ToasterProvider>
+            <BasketProvider>
+                <App />
+            </BasketProvider>
+        </ToasterProvider>
     </StrictMode>
 );
