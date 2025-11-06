33077d5307399f5c59df9c3d5c746c6d1bc4fd77
Cal <callumburgoyne04@gmail.com>
2025-11-06 23:55:57 +0000

    feat: add components for displaying toasts

---
+++ frontend/src/layout/Toaster/ToasterToast.tsx
@@ -0,0 +1,37 @@
+import Icon from "@/components/Icon/Icon";
+import { DarkText } from "@/components/Text/DarkText";
+import { LightText } from "@/components/Text/LightText";
+import { useToaster, type Toast } from "@/context/ToasterContext";
+
+interface ToasterToastProps {
+    toast: Toast;
+}
+
+const ToasterToast: React.FC<ToasterToastProps> = ({ toast }) => {
+    const { removeToast } = useToaster();
+
+    return (
+        <span
+            className={`border-layout-border flex w-full justify-between gap-4 rounded-md border-1 bg-[#ffffffee] p-3 transition-opacity duration-500 sm:w-72 ${toast.removing ? "opacity-0" : "opacity-100"}`}
+        >
+            <span className="flex gap-3">
+                <Icon icon="info" className="mt-0.5 text-xs select-none" />
+                <div className="flex flex-col">
+                    <DarkText className="text-sm">{toast.title}</DarkText>
+                    <LightText className="text-sm">
+                        {toast.description}
+                    </LightText>
+                </div>
+            </span>
+            {toast.closeable && (
+                <Icon
+                    icon="close"
+                    className="hover:text-charcoal text-light-text h-max text-xs transition-colors duration-200 select-none"
+                    onClick={() => removeToast(toast.id)}
+                />
+            )}
+        </span>
+    );
+};
+
+export default ToasterToast;
