c041145849a8d6cbe7a6a1c9572e710f5574dc30
Cal <callumburgoyne04@gmail.com>
2025-11-07 16:02:44 +0000

    feat: update toast styling for different types

--- frontend/src/layout/Toaster/ToasterToast.tsx
+++ frontend/src/layout/Toaster/ToasterToast.tsx
@@ -1,7 +1,8 @@
 import Icon from "@/components/Icon/Icon";
 import { DarkText } from "@/components/Text/DarkText";
 import { LightText } from "@/components/Text/LightText";
-import { useToaster, type Toast } from "@/context/ToasterContext";
+import { ToastType, useToaster, type Toast } from "@/context/ToasterContext";
+import { useMemo } from "react";
 
 interface ToasterToastProps {
     toast: Toast;
@@ -10,20 +11,53 @@ interface ToasterToastProps {
 const ToasterToast: React.FC<ToasterToastProps> = ({ toast }) => {
     const { removeToast } = useToaster();
 
+    const toastStyles = useMemo(() => {
+        switch (toast.type) {
+            case ToastType.Success:
+                return {
+                    bgColor: "bg-green-50",
+                    titleColor: "text-green-700!",
+                    descColor: "text-green-600!",
+                    icon: "check_circle",
+                };
+            case ToastType.Error:
+                return {
+                    bgColor: "bg-red-50",
+                    titleColor: "text-red-700!",
+                    descColor: "text-red-600!",
+                    icon: "error",
+                };
+            case ToastType.Info:
+                return {
+                    bgColor: "bg-white",
+                    descColor: "",
+                    titleColor: "",
+                    icon: "info",
+                };
+        }
+    }, [toast]);
+
     return (
         <span
-            className={`border-layout-border toast-fade-in flex w-full justify-between gap-4 rounded-md border-1 bg-[#ffffffee] p-3 transition-opacity duration-500 sm:w-72 ${toast.removing ? "opacity-0" : "opacity-100"}`}
+            className={`toast-fade-in flex w-full justify-between gap-4 rounded-md p-3 shadow-md transition-opacity duration-500 sm:w-64 ${toast.removing ? "opacity-0" : "opacity-100"} ${toastStyles.bgColor}`}
         >
             <span className="flex gap-3">
-                <Icon icon="info" className="mt-0.5 text-xs select-none" />
+                <Icon
+                    icon={toastStyles.icon}
+                    className={`mt-0.5 text-xs select-none ${toastStyles.titleColor}`}
+                />
                 <div className="flex flex-col">
-                    <DarkText className="text-sm">{toast.title}</DarkText>
-                    <LightText className="text-sm">
+                    <DarkText className={`text-sm ${toastStyles.titleColor}`}>
+                        {toast.title}
+                    </DarkText>
+                    <LightText
+                        className={`text-sm font-light ${toastStyles.descColor}`}
+                    >
                         {toast.description}
                     </LightText>
                 </div>
             </span>
-            {toast.closeable && (
+            {toast.timeout === 0 && (
                 <Icon
                     icon="close"
                     className="hover:text-charcoal text-light-text h-max text-xs transition-colors duration-200 select-none"
