be14d4547647715aa9d783135f4729a00dc61509
Cal <callumburgoyne04@gmail.com>
2025-11-07 16:02:26 +0000

    fix: decrease padding on small screens to prevent wrapping

--- frontend/src/components/SizeGuidePopup/SizeGuidePopupTableRow.tsx
+++ frontend/src/components/SizeGuidePopup/SizeGuidePopupTableRow.tsx
@@ -14,7 +14,7 @@ const SizeGuidePopupTableRow: React.FC<SizeGuidePopupTableRowProps> = ({
             className={`border-input-border grid w-full grid-cols-[2fr_3fr_3fr] border-1 border-b-0 last:border-b-1 ${index % 2 == 0 ? "bg-layout-border" : "bg-background"} ${index === 0 && "font-semibold"}`}
         >
             {values.map((v) => (
-                <DarkText className="border-input-border border-r-1 px-12 py-2 text-center text-sm last:border-r-0">
+                <DarkText className="border-input-border border-r-1 px-6 py-2 text-center text-sm last:border-r-0 sm:px-12">
                     {v}
                 </DarkText>
             ))}
