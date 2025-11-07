e9291e63ea8e2e94e628ef4768b2dba98cb6d3bd
Cal <callumburgoyne04@gmail.com>
2025-11-07 16:01:09 +0000

    feat: increase z index to above other elements

--- frontend/src/layout/Toaster/Toaster.tsx
+++ frontend/src/layout/Toaster/Toaster.tsx
@@ -4,9 +4,9 @@ import ToasterToast from "./ToasterToast";
 const Toaster = () => {
     const { toasts } = useToaster();
     return (
-        <div className="fixed right-2 bottom-2 z-99 flex w-[calc(100vw-16px)] flex-col gap-2 sm:right-4 sm:bottom-4 sm:w-max sm:gap-4">
+        <div className="fixed right-2 bottom-2 z-50 flex w-[calc(100vw-16px)] flex-col gap-2 sm:right-4 sm:bottom-4 sm:w-max sm:gap-4">
             {toasts.map((t) => (
-                <ToasterToast toast={t} />
+                <ToasterToast key={t.id} toast={t} />
             ))}
         </div>
     );
