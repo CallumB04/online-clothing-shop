32655f4a73670cca959061cd3ad91daeb05b6078
Cal <callumburgoyne04@gmail.com>
2025-11-11 20:10:56 +0000

    feat: add basket calculation function

---
+++ backend/internal/checkout/checkout.go
@@ -0,0 +1,49 @@
+package checkout
+
+import (
+	"errors"
+
+	"github.com/callumb04/clothing-shop/backend/internal/data"
+	"github.com/callumb04/clothing-shop/backend/internal/models"
+	"github.com/callumb04/clothing-shop/backend/internal/util"
+)
+
+var ErrEmptyBasket = errors.New("empty basket")
+
+// Calculate total price of basket
+func CalculateBasketTotal(basket models.Basket, discount *float64) (float64, error) {
+	// Return no price if empty basket
+	if len(basket) == 0 {
+		return 0.0, ErrEmptyBasket
+	}
+
+	items, err := data.LoadItems()
+
+	// Return error if items failed to load
+	if err != nil {
+		return 0.0, err
+	}
+
+	var total float64 = 0
+
+	// Iterate through all items in basket
+	for _, bi := range basket {
+		// Find item data of that basket item and add to total
+		for _, i := range items {
+			if i.ID == bi.ItemID {
+				if i.DiscountPriceGBP != nil {
+					total += (*i.DiscountPriceGBP * float64(bi.Quantity)) // Add discount price if exists
+				} else {
+					total += (i.PriceGBP * float64(bi.Quantity)) // Add full price
+				}
+			}
+		}
+	}
+
+	if discount != nil {
+		discountTotal := CalculateBasketTotalAfterDiscount(total, *discount)
+		return util.RoundTo2DP(discountTotal), nil
+	}
+
+	return util.RoundTo2DP(total), nil
+}
