bbb1bfb36a7320ee47b45c65a1da739f1d398979
Cal <callumburgoyne04@gmail.com>
2025-11-12 02:06:18 +0000

    feat: create helper function for logging http requests

---
+++ backend/internal/util/logger.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+// Logs basic info from http requests to backend
+func LogRequest(logger *zap.Logger, r *http.Request, startTime time.Time) {
+	// Calculate time since request was received, giving time taken for response
+	duration := time.Since(startTime).Milliseconds()
+
+	// Logging basic request info
+	logger.Info("new request",
+		zap.String("method", r.Method),
+		zap.String("path", r.URL.Path),
+		zap.String("duration", fmt.Sprintf("%dms", duration)),
+	)
+}
