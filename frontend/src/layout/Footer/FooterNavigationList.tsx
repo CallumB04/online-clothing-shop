cb73047fd09d695dd57ede591514f0a1653d75ed
Cal <callumburgoyne04@gmail.com>
2025-11-14 23:18:57 +0000

    feat: add component for column in footer navigation

---
+++ frontend/src/layout/Footer/FooterNavigationList.tsx
@@ -0,0 +1,37 @@
+// export interface FooterNavigationOption = {
+
+import { LightText } from "@/components/Text/LightText";
+import { WhiteText } from "@/components/Text/WhiteText";
+import { Link } from "react-router-dom";
+
+// }
+
+interface FooterNavigationListProps {
+    header: string;
+    options: { label: string; to: string }[];
+}
+
+const FooterNavigationList: React.FC<FooterNavigationListProps> = ({
+    header,
+    options,
+}) => {
+    return (
+        <div className="flex flex-col gap-4 text-left">
+            <WhiteText className="font-semibold tracking-wide">
+                {header}
+            </WhiteText>
+            {/* Section options */}
+            <div className="flex flex-col gap-2">
+                {options.map((o) => (
+                    <Link to={o.to}>
+                        <LightText className="font-light transition-colors duration-200 hover:text-white">
+                            {o.label}
+                        </LightText>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default FooterNavigationList;
