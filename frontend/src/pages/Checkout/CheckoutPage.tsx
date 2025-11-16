09982d42c9ce73804152e04130694b4cf3da1cba
Cal <callumburgoyne04@gmail.com>
2025-11-16 01:21:02 +0000

    feat: add basic checkout page layout

--- frontend/src/pages/Checkout/CheckoutPage.tsx
+++ frontend/src/pages/Checkout/CheckoutPage.tsx
@@ -1,4 +1,11 @@
+import Card from "@/components/Card/Card";
+import Divider from "@/components/Divider/Divider";
+import Icon from "@/components/Icon/Icon";
+import PageHeader from "@/components/PageHeader/PageHeader";
+import LightClickableText from "@/components/Text/LightClickableText";
 import DefaultSidebar from "@/layout/DefaultSidebar/DefaultSidebar";
+import { Link } from "react-router-dom";
+import CheckoutPageBasketTable from "./components/CheckoutPageBasketTable";
 
 interface CheckoutPageProps {
     isMobileSidebarOpen?: boolean;
@@ -8,7 +15,32 @@ const CheckoutPage: React.FC<CheckoutPageProps> = ({ isMobileSidebarOpen }) => {
     return (
         <>
             <DefaultSidebar open={isMobileSidebarOpen} />
-            <main className="flex flex-col gap-8 px-2 py-4 sm:px-4 sm:py-6"></main>
+            <main className="mx-auto flex max-w-360 flex-col gap-8 px-2 py-4 sm:px-4 sm:py-6">
+                {/* Back to shop text */}
+                <Link to="/shop" className="group flex items-center gap-1">
+                    <Icon
+                        icon="chevron_left"
+                        className="text-light-text group-hover:text-charcoal text-sm transition-colors duration-200"
+                    />
+                    <LightClickableText>Back to shop</LightClickableText>
+                </Link>
+                <div className="flex w-full flex-col gap-6 xl:flex-row">
+                    {/* Basket + details (left) */}
+                    <Card className="flex w-full flex-col gap-4 xl:min-w-0 xl:flex-1">
+                        {/* Basket */}
+                        <PageHeader text="Your Basket" />
+                        <CheckoutPageBasketTable />
+                        <Divider />
+                        {/* Details */}
+                        <PageHeader text="Your Details" />
+                    </Card>
+
+                    {/* Checkout (right) */}
+                    <Card className="flex w-full flex-col gap-4 xl:w-80 xl:flex-none">
+                        <PageHeader text="Checkout" />
+                    </Card>
+                </div>
+            </main>
         </>
     );
 };
